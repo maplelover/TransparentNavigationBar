63f3cabd329da549506137fd0195f68b3ead9fe6
Maple <278390341@qq.com>
2015-08-24 01:19:24 +0800

    Hides Navigation bar

---
+++ UIViewController+NavigationBar.h
@@ -0,0 +1,16 @@
+//
+//  UIViewController+NavigationBar.h
+//  zhoujr
+//
+//  Created by zhoujr on 15/8/24.
+//  Copyright (c) 2015年 Topvogues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (NavigationBar)
+
+- (void)setNavigationBarTransparent:(BOOL)transparent;
+- (void)setNavigationBarHidden:(BOOL)hidden;
+
+@end
